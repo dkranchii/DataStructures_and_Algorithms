0fb7b3a9905c33edc986edb85bdd8c3b656fecac
tigerkdp <tigerkdp@gmail.com>
2018-10-22 10:41:49 -0700

    Heap

--- Heap/heap.py
+++ Heap/heap.py
@@ -203,6 +203,30 @@ print(H.remove_max())
 
 
 
+"""
+#------Output--------#
+
+Heap contains inf 0 0 0 0 0 0 0 0 0 
+Heap contains inf 77 66 45 12 11 14 23 9 4 0 0 0 0 0 0 0 0 0 0 
+Max Value in the heap is 77
+Heap contains inf 77 66 62 12 63 45 23 9 4 11 43 14 9 0 0 0 0 0 0 
+
+Get Max Values
+77
+66
+63
+62
+45
+43
+23
+14
+12
+11
+9
+9
+4
+
+"""
 
 
         
