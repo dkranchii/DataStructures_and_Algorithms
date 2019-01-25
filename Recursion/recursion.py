66c41eb64d01deeeabe5b6173d4cfff831604aab
tigerkdp <tigerkdp@gmail.com>
2019-01-24 22:04:27 -0800

    Recursion

--- Recursion/recursion.py
+++ Recursion/recursion.py
@@ -163,31 +163,31 @@ print(print_digits_reversed_vert(1234))
 #2
 #1
 
-def max_list_length_DAC1(A):
+def max_item_in_list_DAC1(A):
     if len(A) == 1:
         return A[0]
     else:
         middle = len(A) // 2
-        m1 = max_list_length_DAC1(A[0:middle])
-        m2 = max_list_length_DAC1(A[middle:len(A)])
+        m1 = max_item_in_list_DAC1(A[0:middle])
+        m2 = max_item_in_list_DAC1(A[middle:len(A)])
         return max(m1, m2)
     
 A = [23, -1, 5, 56, 4, 2, 7]
-print("max_list_length_DAC1", max_list_length_DAC1(A))
+print("max_list_length_DAC1", max_item_in_list_DAC1(A))
 #max_list_length_DAC1 56
 
 
-def max_list_length_DAC2(A, lower, upper):
+def max_item_in_list_DAC2(A, lower, upper):
     if lower == upper:
         return A[lower]
     else:
         middle = (upper + lower) // 2
-        m1 = max_list_length_DAC2(A, lower, middle)
-        m2 = max_list_length_DAC2(A, middle +1, upper)
+        m1 = max_item_in_list_DAC2(A, lower, middle)
+        m2 = max_item_in_list_DAC2(A, middle +1, upper)
         return max(m1, m2)
     
 A = [23, -1, 5, 5, 4, 2, 7]
-print("max_list_length_DAC2", max_list_length_DAC2(A, 0, len(A)-1))
+print("max_list_length_DAC2", max_item_in_list_DAC2(A, 0, len(A)-1))
 #max_list_length_DAC2 23
 
 def is_even(n):
@@ -257,15 +257,173 @@ print("is palindrome kasam", is_palindrome("kasam"))
 #is palindrome kasam False
 
 
+def equal_strings(s,t):
+    if len(s) != len(t):
+        return False
+    elif s == '':
+        return True
+    else:
+        return s[0] == t[0] and equal_strings(s[1:], t[1:])
+    
+print("strings joe and joe are equal", equal_strings( "joe", "joe"))
+print("strings smith and joe are equal", equal_strings( "smith", "joe"))
+#strings joe and joe are equal True
+#strings smith and joe are equal False
+
+def equal_strings_tail(s, t):
+    if len(s) != len(t):
+        return False
+    elif s == '':
+        return True
+    elif s[0] != t[0]:
+        return False
+    else:
+        return equal_strings_tail(s[1:], t[1:])
+    
+
+print("strings mary and mary are equal", equal_strings_tail( "mary", "mary"))
+print("strings mary and joe are equal", equal_strings_tail( "mary", "joe"))
+#strings mary and mary are equal True
+#strings mary and joe are equal False
+
 def occurrences_in_list(A, x):
     if A == []:
         return 0
     else:
         return int(A[0] == x) + occurrences_in_list(A[1:],x)
     
-A=[1,34,5,2,-1,23,2]
-print("occurrences_in_list", occurrences_in_list(A, 2))
+A=[1,34,5,2,3,23,2]
+print("occurrences_in_list", occurrences_in_list(A,3))
 #occurrences_in_list 2
 
+def linear_search_list_tail(A, x):
+    n = len(A)
+    if A==[]:
+        return -1
+    elif A[n-1] == x:
+        return n-1
+    else:
+        return linear_search_list_tail(A[:n-1])
+    
+    
+A=[1,34,5,12,4,23,2]
+print("linear search in list", linear_search_list_tail(A,2))
+#linear search in list 6
+    
+def linear_search_linear(A, x, n):
+    if A == []:
+        return -n-1
+    elif A[0] == x:
+        return 0
+    else:
+        return 1 + linear_search_linear(A[1:], x, n)
+    
+    
+A=[1,34,5,12,5,23,2]
+n = len(A)
+print("linear search in list", linear_search_linear(A, 12, n))
+#linear search in list 3
+print("linear search in list", linear_search_linear(A, 15, n))
+#linear search in list -1
 
+def linear_search_tail_2(A, x, n):
+    if A == []:
+        return -1
+    elif A[0] == x:
+        return n
+    else:
+        return 1 + linear_search_tail_2(A[1:], x, n+1)
+    
+    
+A=[1,34,5,12,4,23,2]
+n = len(A)
+print("linear search in list", linear_search_linear(A, 23, n))
+#linear search in list 5
+print("linear search in list", linear_search_linear(A, 11, n))
+#linear search in list -1
+
+def binary_search(A, x, lower, upper):
+    if lower > upper:
+        return -1
+    else:
+        middle = (lower + upper) // 2
+        if x== A[middle]:
+            return middle
+        elif x < A[middle]:
+            return binary_search(A, x, lower, middle-1)
+        else:
+            return binary_search(A, x, middle+1, upper)
+
+bb=[1, 3, 7, 22, 33, 44, 55, 56, 78]
+print("binary search in a list" , binary_search(bb, 56, 0, len(bb)-1))
+#binary search in a list 7
+
+def get_smaller_than_or_equal_to(A, x):
+    if A == []:
+        return []
+    elif A[0] <= x:
+        return [A[0]] + get_smaller_than_or_equal_to(A[1:], x)
+    else:
+        return get_smaller_than_or_equal_to(A[1:],x)
+    
+AA = [9,6,1,7,4,5]
+print("get smaller than 5", get_smaller_than_or_equal_to(AA, 5))
+#get smaller than 5 [1, 4, 5]
+
+def get_greater_than(A, x):
+    if A == []:
+        return []
+    elif A[0] > x:
+        return [A[0]] + get_greater_than(A[1:],x)
+    else:
+        return get_greater_than(A[1:],x)
+    
+AB = [9,6,1,7,4,5]
+print("get greater than 5", get_greater_than(AB, 4))
+#get greater than 5 [9, 6, 7, 5] 
+
+
+def select_sort_rec(a):
+    if len(a) <= 1:
+        return a
+    else:
+        b = list(a)
+        m = min(b)
+        b.remove(m)
+        
+        return [m] + select_sort_rec(b)
+    
+A = [7,5,23,3,8,4]
+print(select_sort_rec(A))
+
+
+def select_sort_rec_1(a):
+    if len(a) <= 1:
+        return a
+    else:
+        b = list(a)
+        min_indx = b.index(min(b))
+        aux = b[min_indx]
+        b[min_indx] = b[0]
+        b[0] = aux
+        
+        return [aux] + select_sort_rec_1(b[1:])
+    
+A = [7,5,23,3,8,4]
+print(select_sort_rec_1(A))
+
+
+def pascal(n):
+    if n==0:
+        return [1]
+    else:
+        row = [1]
+        previous_row = pascal(n-1)
+        for i in range(len(previous_row)-1):
+            row.append(previous_row[i] + previous_row[i+1])
+        row.append(1)
+    return row
+
+print(pascal(8))
+#[1, 8, 28, 56, 70, 56, 28, 8, 1]
 
